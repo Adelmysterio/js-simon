c008a88a26c8878db2e84739de0ca8125e30087d
AdelMysterio <storliniadelmo@gmail.com>
2024-05-21 13:59:33 +0200

    scritto timeout da debuggare

--- js/script.js
+++ js/script.js
@@ -16,3 +16,18 @@ while (randomNumbers.length != 5) {
         liElement.append(randomInt);
     }
 };
+
+setTimeout (function(){
+    output.innerHTML = ''
+    for (i=0; i<5; i++) {
+        let userInt = Number.parseInt(prompt('Scrivi un numero di secondi da attendere'), 10);
+        userNumbers.push(userInt)
+    }
+
+    for (i=0; i<randomNumbers; i++) {
+        if (userNumbers.includes(randomNumbers[i])) {
+            equalNumbers.push(randomNumbers[i])
+        }
+    }
+    console.log(equalNumbers, equalNumbers.length)
+},3000)
